44f0ceb3b8e8a96573a67e5bdd8091a2542e2bfe
Hector  Ortega <103329355+yoo-hec@users.noreply.github.com>
2025-04-15 22:21:26 -0700

    Update server.js

--- server.js
+++ server.js
@@ -3,8 +3,11 @@
 const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 3000;
+const path = require("path");
 
 // add any necessary code you'd want to!
+
+app.use(express.static(path.join(__dirname, "public")));
 
 //root
 app.get('/', (req, res) => {
@@ -15,18 +18,21 @@ app.get('/', (req, res) => {
 
 // Route for Climate Crisis
 app.get("/climate-crisis", (req, res) => {
-  //TODO add code!
-});
-
-// Route for Typesetting
-app.get("/typesetting", (req, res) => {
-  //TODO add code
-});
-
-// Route for Four Algorithms
-app.get("/four-algorithms", (req, res) => {
-  //TODO add code
-});
+    //TODO add code!
+      res.redirect("https://github.com/yoo-hec/Climate-Crisis");
+  });
+  
+  // Route for Typesetting
+  app.get("/typesetting", (req, res) => {
+    //TODO add code
+      res.redirect("https://github.com/yoo-hec/Typesetting");
+  });
+  
+  // Route for Four Algorithms
+  app.get("/four-algorithms", (req, res) => {
+    //TODO add code 
+      res.redirect("https://github.com/yoo-hec/CSC-317");
+  });
 
 // Start the server
 app.listen(PORT, () => {
